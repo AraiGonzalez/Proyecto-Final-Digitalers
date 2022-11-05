ab4f017ea1a72253e73d9671e3553d15abaa72fb
Arai Gonzalez <araiestefania94hotmail.com>
2022-11-04 21:48:51 -0300

    proyecto base

---
+++ src/test/java/com/example/DigitalersProyectoFinal/DigitalersProyectoFinalApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.DigitalersProyectoFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+
+class DigitalersProyectoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
