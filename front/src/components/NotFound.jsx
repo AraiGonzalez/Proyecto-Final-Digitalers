37946ad56f0b1ce0b02ccc98a906f719a68a29df
AraiGonzalez <araiestefania94@hotmail.com>
2022-11-16 23:37:56 -0300

    Create NotFound.jsx

---
+++ front/src/components/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../resources/css/error.css';
+
+export default function NotFound(props) {
+    return(
+        <div>
+            <p>Ups... Error 404, Recurso no encontrado</p>
+        </div>
+    
+    );
+}
