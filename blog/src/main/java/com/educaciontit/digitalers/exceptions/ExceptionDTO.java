e220cc7ed18cd5e3136b555873b95a3a10952ee6
AraiGonzalez <araiestefania94@hotmail.com>
2022-11-16 23:43:55 -0300

    Create ExceptionDTO.java

---
+++ blog/src/main/java/com/educaciontit/digitalers/exceptions/ExceptionDTO.java
@@ -0,0 +1,11 @@
+package com.educaciontit.digitalers.exceptions;
+
+public class ExceptionDTO extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ExceptionDTO(String message) {
+		super(message);
+	}
+
+}
